dc3c67375b11553dc952827393be668769b68f8c
Ralphius <sod@off.com>
2020-04-17 21:13:40 +0100

    Trying to sync, don't know what's hapenning anymore...

--- motor_sense_load.py
+++ motor_sense_load.py
@@ -7,14 +7,18 @@ BP = brickpi3.BrickPi3()
 BP.reset_all()
 
 try:
+    desiredSpeed = 165 * 6
     BP.reset_motor_encoder(BP.PORT_C)
-    BP.set_motor_limits(BP.PORT_C, 50, 0)
-    BP.set_motor_dps(BP.PORT_C, 250)
+    BP.set_motor_limits(BP.PORT_C, 0, 0)
+    BP.set_motor_dps(BP.PORT_C, desiredSpeed)
+    atSteadyState = False
     while True:
         try:
             status = BP.get_motor_status(BP.PORT_C)
             print("flags: {0}\tpwr: {1}\t enc: {2}\tdps: {3}".format(status[0], status[1], status[2], status[3]))
-            if (status[0] == 2):
+            if (atSteadyState == False and status[3] == desiredSpeed):
+                atSteadyState = True
+            if (atSteadyState == True and status[3] < 170 * 3):
                 BP.set_motor_power(BP.PORT_C, -128) # float
                 break
         except IOError as error:
