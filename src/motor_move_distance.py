a04447c427f536b30bec0d69c7989859b82b0c93
ralph <ralph@HP-Pavilion-broken-screen.lan>
2020-04-24 20:49:36 +0100

    Halfway to implementing logo/turtle mode haha!

--- src/motor_move_distance.py
+++ src/motor_move_distance.py
@@ -12,28 +12,71 @@ BP = brickpi3.BrickPi3()
 # Constants
 WHEEL_DIAMETER = 8.2 # CM
 GEAR_RATIO = 12 / 20  # 12t:20t
+WHEEL_TRACK_WIDTH = 15.00 # CM (nice round number, what are the chances!)
 
-def move_distance(distance):
-    try:
-        print("Moving {} cm...".format(distance))
-        wheel_circumference = math.pi * WHEEL_DIAMETER
-        num_wheel_revolutions = distance / wheel_circumference
-        print("Wheel revolutions: {}".format(num_wheel_revolutions))
+LEFT_MOTOR = BP.PORT_B
+RIGHT_MOTOR = BP.PORT_C
 
-        num_motor_revolutions = num_wheel_revolutions / GEAR_RATIO
-        motor_degrees = 360 * num_motor_revolutions
-        print("Motor degrees: {}".format(motor_degrees))
-        BP.set_motor_position_relative(BP.PORT_B, motor_degrees)
-        BP.set_motor_position_relative(BP.PORT_C, motor_degrees)
-        time.sleep(5)
-        BP.set_motor_power(BP.PORT_B, -128)
-        BP.set_motor_power(BP.PORT_C, -128)
-    except KeyboardInterrupt:
-        BP.reset_all()
-        print ("Bye")
 
+def distance_to_motor_degrees(distance):
+    # print("Moving {} cm...".format(distance))
+    wheel_circumference = math.pi * WHEEL_DIAMETER
+    num_wheel_revolutions = distance / wheel_circumference
+    # print("Wheel revolutions: {}".format(num_wheel_revolutions))
+    num_motor_revolutions = num_wheel_revolutions / GEAR_RATIO
+    motor_degrees = 360 * num_motor_revolutions
+    # print("Motor degrees: {}".format(motor_degrees))
+    return motor_degrees
+    
+    
+def turn_left(angle):
+    fraction_of_circle = angle / 360
+    required_distance = fraction_of_circle * (math.pi * WHEEL_TRACK_WIDTH)
+    motor_degrees = distance_to_motor_degrees(required_distance)
+    BP.set_motor_power(LEFT_MOTOR, 0)
+    BP.set_motor_position_relative(RIGHT_MOTOR, motor_degrees)
+    
+def turn_right(angle):
+    fraction_of_circle = angle / 360
+    required_distance = fraction_of_circle * (math.pi * WHEEL_TRACK_WIDTH)
+    motor_degrees = distance_to_motor_degrees(required_distance)
+    BP.set_motor_power(LEFT_MOTOR, motor_degrees)
+    BP.set_motor_position_relative(RIGHT_MOTOR, 0)
+
+def move_forward(distance):
+    motor_degrees = distance_to_motor_degrees(distance)
+    BP.set_motor_position_relative(LEFT_MOTOR, motor_degrees)
+    BP.set_motor_position_relative(RIGHT_MOTOR, motor_degrees)
+
+def move_backward(distance):
+    motor_degrees = distance_to_motor_degrees(distance)
+    BP.set_motor_position_relative(LEFT_MOTOR, -motor_degrees)
+    BP.set_motor_position_relative(RIGHT_MOTOR, -motor_degrees)
+
+def wait_for_motors_to_stop():
+    both_stopped = false
+    while (not both_stopped):
+        time.sleep(0.1)
+        left_motor_dps = BP.get_motor_status(LEFT_MOTOR)[3]
+        right_motor_dps = BP.get_motor_status(RIGHT_MOTOR)[3]
+        if (left_motor_dps < 1 and right_motor_dps < 1):
+            both_stopped = true
+        
+        
 if __name__ == "__main__":
     BP.set_motor_limits(BP.PORT_B, 40, 600)
     BP.set_motor_limits(BP.PORT_C, 40, 600)
     required_distance = float(input("Enter number of centimeters to move: "))
-    move_distance(required_distance)
+    try:
+        move_forward(required_distance)
+        wait_for_motors_to_stop()
+        turn_left(30)
+        wait_for_motors_to_stop()
+        turn_right(30)
+        wait_for_motors_to_stop()
+        move_backward(required_distance)
+        wait_for_motors_to_stop()
+    except KeyboardInterrupt:
+        BP.reset_all()
+        print ("Bye")
+        
