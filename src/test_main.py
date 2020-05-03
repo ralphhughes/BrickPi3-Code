112b1990412f881b52164f7989e68b923a580cec
ralph <ralphhughes@hotmail.com>
2020-05-03 14:59:15 +0100

    Much better algorithm now. We'll call this "first successful prototype"

--- src/test_main.py
+++ src/test_main.py
@@ -1,6 +1,8 @@
 from lib import robot_movement
-
-# Testing environment dependent importing
+import time
+import os
+import random
+import threading
 import platform
 if platform.system() == 'Windows':
     from lib import brickpi3_mock
@@ -9,67 +11,108 @@ elif platform.system() == 'Linux':
     import brickpi3
     BP = brickpi3.BrickPi3()
 
-
-from time import sleep
+LEFT_MOTOR = BP.PORT_B
+RIGHT_MOTOR = BP.PORT_C
+M = robot_movement.Movement(BP, LEFT_MOTOR, RIGHT_MOTOR)
+MOTORS_ENABLED = True
 
 from Bluetin_Echo import Echo # https://github.com/MarkAHeywood/Bluetin_Python_Echo
-
 # Define pin constants
 TRIGGER_PIN_LEFT = 27
 ECHO_PIN_LEFT = 17
 TRIGGER_PIN_RIGHT = 22
 ECHO_PIN_RIGHT = 23
-
-# Initialise two sensors.
+# Initialise two HC-SR04 sensors on Pi GPIO pins.
 echo = [Echo(TRIGGER_PIN_LEFT, ECHO_PIN_LEFT)
-        , Echo(TRIGGER_PIN_RIGHT, ECHO_PIN_RIGHT)]
+    , Echo(TRIGGER_PIN_RIGHT, ECHO_PIN_RIGHT)]
 
-def main():
+# Initialise NXT ultrasonic on BrickPi3 sensor port
+BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.NXT_ULTRASONIC)
+
+
+
+def safeQuit():
+    echo[0].stop()
+    echo[1].stop()
+    print("Both sonars stopped")
+    BP.set_motor_dps(LEFT_MOTOR, 0)
+    BP.set_motor_dps(RIGHT_MOTOR, 0)
+    BP.reset_all()
+    print("Both motors stopped")
+    time.sleep(3)
+    os._exit(0)
+
+def main(name):
     try:
-        M = robot_movement.Movement(BP)
-        BP.set_motor_limits(BP.PORT_B, 45, 900)
-        BP.set_motor_limits(BP.PORT_C, 45, 900)
-        
-        BP.set_motor_dps(BP.PORT_B, 200)
-        BP.set_motor_dps(BP.PORT_C, 200)
-        while True:
+        if MOTORS_ENABLED:
+            BP.set_motor_limits(LEFT_MOTOR, 45)
+            BP.set_motor_limits(RIGHT_MOTOR, 45)
+            BP.set_motor_dps(LEFT_MOTOR, 200)
+            BP.set_motor_dps(RIGHT_MOTOR, 200)
 
+        while True:
             left_distance = echo[0].read('cm')
             right_distance = echo[1].read('cm')
-            print('left: {}cm\tright: {}cm'.format(round(left_distance,0), round(right_distance,0)))
+            try:
+                center_distance = BP.get_sensor(BP.PORT_1)
+            except brickpi3.SensorError:
+                print("ERROR: Can't read NXT ultrasonic sensor!")
+                center_distance = 256
+            time.sleep(0.2) # 0.029
 
-            # 0 for out of range?
-            
+            # Bluetin_Echo uses '0' as 'OutOfRange', BrickPi3 uses 255
+            if left_distance == 0 or left_distance > 255:
+                left_distance = 255
+            if right_distance == 0 or right_distance > 255:
+                right_distance = 255
+
+
+            # Default action
             action = 'forward'
             
-            if left_distance < 20 and left_distance > 0:
-                action='right'
-            
-            if right_distance < 20 and right_distance > 0:
-                action='left'
-            
-            print(action)
-            
-            if action == 'forward':
-                BP.set_motor_dps(BP.PORT_B, 200)
-                BP.set_motor_dps(BP.PORT_C, 200)
-            if action == 'right':
-                M.rotate_right(45)
-                M.wait_for_motors_to_stop()
-            elif action == 'left':
-                M.rotate_left(45)
-                M.wait_for_motors_to_stop()
-                
+            if left_distance < 20 or right_distance < 20:
+                if left_distance < right_distance:
+                    action='right'
+                else:
+                    action='left'
+
+            if center_distance < 20:
+                action='reverse'
+
+            print('left: {}cm\tcenter: {}cm\tright: {}cm\taction: {}'.format(
+                int(left_distance), int(center_distance), int(right_distance), action)
+            )
+
+            if MOTORS_ENABLED:
+                if action == 'forward':
+                    BP.set_motor_dps(LEFT_MOTOR, 3 * min(left_distance,right_distance,center_distance))
+                    BP.set_motor_dps(RIGHT_MOTOR, 3 * min(left_distance,right_distance,center_distance))
+                elif action == 'reverse':
+                    M.move_backward(30)
+                    M.wait_for_motors_to_stop()
+                    if random.choice([True, False]):
+                        M.rotate_left(90)
+                        M.wait_for_motors_to_stop()
+                    else:
+                        M.rotate_right(90)
+                        M.wait_for_motors_to_stop()
+                elif action == 'right':
+                    M.rotate_right(90)
+                    M.wait_for_motors_to_stop()
+                elif action == 'left':
+                    M.rotate_left(90)
+                    M.wait_for_motors_to_stop()
+
     except KeyboardInterrupt:
-        echo[0].stop()
-        echo[1].stop()
-        print("Both sonars stopped")
-        BP.set_motor_dps(BP.PORT_B, 0)
-        BP.set_motor_dps(BP.PORT_C, 0)
-        BP.reset_all()
-        print("Both motors stopped")
-        time.sleep(3)
+        safeQuit()
         
 if __name__ == '__main__':
-    main()
+    x = threading.Thread(target=main, args=(1,), daemon=True)
+    x.start()
+    try:
+        t=input("Type any letter then press enter to quit")
+    except KeyboardInterrupt:
+        safeQuit()
+    safeQuit()
+
 
